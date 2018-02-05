0aa9807ac5222e20113293bf34e9f3f07cba160c
Pavan Sethi <pavansethi@Pavans-MacBook-Pro.local>
2018-02-05 14:56:46 -0800

    First commit

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-  database: process.env.DATABASE || '',
+  database: process.env.DATABASE || 'mongodb://root:tiger216@ds157987.mlab.com:57987/fiverrdatabase',
   port: process.env.PORT || 3000,
-  secret: process.env.SECRET || '',
+  secret: process.env.SECRET || 'fiverrclone',
 
 }
