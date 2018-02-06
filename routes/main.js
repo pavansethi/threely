f9d626761303f0a7afad6c8a4004a595071ef3c8
Pavan Sethi <pavansethi@Pavans-MacBook-Pro.local>
2018-02-05 18:03:18 -0800

    Adds route for my-gigs

--- routes/main.js
+++ routes/main.js
@@ -4,4 +4,8 @@ router.get('/', (req, res, next) => {
     res.render('main/home');
 });
 
+router.get('/mygigs', (req, res, next) => {
+  res.render('main/my-gigs');
+});
+
 module.exports = router;
