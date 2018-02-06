ffa5118d5e4d2ba1810ba2df851a1730613785a6
Pavan Sethi <pavansethi@Pavans-MacBook-Pro.local>
2018-02-05 18:44:30 -0800

    Adds route for adding a new gig

--- routes/main.js
+++ routes/main.js
@@ -8,4 +8,9 @@ router.get('/my-gigs', (req, res, next) => {
   res.render('main/my-gigs');
 });
 
+router.route('/add-new-gig')
+  .get((req, res, next) => {
+    res.render('main/add-new-gig');
+  });
+
 module.exports = router;
